ddd9b34b6858f4ca3539adeb669d7b8a7f4539c0
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-11-03 14:32:14 +0000

    plotting

--- sub-salt-avo.py
+++ sub-salt-avo.py
@@ -34,5 +34,9 @@ well_in = {"Vp": 3000, "Vs": 1500, "Rho": 2.3}
 model_in = {"Q_sls": 10, "Log_omega_ref": np.log10(35)}
 inputs = {**well_in, **model_in}
 model = manRP.models(identifier="sls", **inputs)
+om = np.linspace(-3, 3, 100)
 
 st.write(f"{model}")
+fig, ax = plt.subplots()
+ax.plot(om, model(om))
+st.pyplot(fig)
