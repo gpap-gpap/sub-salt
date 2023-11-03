03e743da6c994d3b9378bb2c0eb8b77e591df2f6
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-11-03 14:34:33 +0000

    update app

--- sub-salt-avo.py
+++ sub-salt-avo.py
@@ -35,8 +35,8 @@ model_in = {"Q_sls": 10, "Log_omega_ref": np.log10(35)}
 inputs = {**well_in, **model_in}
 model = manRP.models(identifier="sls", **inputs)
 om = np.linspace(-3, 3, 100)
-c11 = np.array([model.Cij(omega=o)[:, 0, 0] for o in om])
+c11 = np.array([model.Cij(omega=o)[0, 0] for o in om])
 st.write(f"{model}")
 fig, ax = plt.subplots()
-ax.plot(om, model.Cij(omega=om))
+ax.plot(om, np.real(c11))
 st.pyplot(fig)
