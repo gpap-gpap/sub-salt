1cf2c5af0149f3e289281be62ec116b71408479a
giorgos papageorgiou <gpap.gpap@gmail.com>
2023-11-03 11:30:09 +0000

    another try

--- sub-salt-avo.py
+++ sub-salt-avo.py
@@ -10,7 +10,10 @@ try:
 
 # This block executes only on the first run when your package isn't installed
 except ModuleNotFoundError as e:
-    st.write(f"worked to module not found")
+    sleep_time = 45
+    dependency_warning = st.warning(
+        f"Installing dependencies, this takes {sleep_time} seconds."
+    )
     subprocess.Popen(
         [
             f"{sys.executable} -m pip install git+https://${st.secrets['git_token']}@github.com/gpap-gpap/anisotroPY.git@dev-fAVO"
@@ -18,7 +21,7 @@ except ModuleNotFoundError as e:
         shell=True,
     )
     # wait for subprocess to install package before running your actual code below
-    time.sleep(90)
+    time.sleep(sleep_time)
     dependency_warning.empty()
 
 # Rest of your code goes here
